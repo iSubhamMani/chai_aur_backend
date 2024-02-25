41d85c94010d44f6e90bb555ce79080b4e97abf1
Subham Mani <manisubham09@gmail.com>
2024-02-25 12:41:36 +0530

    connect to mongodb

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectToDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+
+        if (connectionInstance) {
+            console.log(
+                "Connected to MongoDB. Host: " +
+                    connectionInstance.connection.host
+            );
+        }
+    } catch (error) {
+        console.log("MongoDB Connection Error: ", error);
+        process.exit(1);
+    }
+};
+
+export default connectToDB;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+import connectToDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env",
+});
+
+connectToDB();
