34a6e31f82b2977d371bd3295b203d508de2e1a3
Subham Mani <manisubham09@gmail.com>
2024-02-25 19:13:21 +0530

    create and configure app

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN, // the url from where the requests are coming
+        credentials: true,
+    })
+);
+
+app.use(
+    express.json({
+        // a middleware function to parse incoming JSON data from HTTP requests
+        limit: "16kb",
+    })
+);
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // a middleware function to parse incoming URL-encoded data from HTTP requests (extended: true allows for rich objects and arrays to be encoded into the URL-encoded format)
+
+app.use(express.static("public")); // a middleware function to serve static files
+
+app.use(cookieParser()); // a middleware function to parse cookies from the HTTP request object
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
 import connectToDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectToDB();
+connectToDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.error(`Error starting app: ${error}`);
+            process.exit(1);
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(
+                `Server is running on port ${process.env.PORT || 8000}`
+            );
+        });
+    })
+    .catch((error) => {
+        console.log(`MongoDB connection failed: ${error}`);
+    });
