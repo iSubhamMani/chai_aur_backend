0491c989787b32f9b3faa5fa294add9eed8de4fe
Subham Mani <manisubham09@gmail.com>
2024-03-04 21:47:05 +0530

    fix cover image upload bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,6 @@ const registerUser = asyncHandler(async (req, res) => {
     // get data from frontend
 
     const { fullName, email, username, password } = req.body;
-    console.log("Email: ", email);
 
     // validation - not empty
 
@@ -33,7 +32,15 @@ const registerUser = asyncHandler(async (req, res) => {
     // file checking - avatar
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required");
