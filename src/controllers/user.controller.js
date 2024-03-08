e8a4420a301c27bad8ef8ecb8c37f05846f19c6a
Subham Mani <manisubham09@gmail.com>
2024-03-08 20:27:21 +0530

    Add getChannelDetails controller function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -292,6 +292,74 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, "Cover image updated successfully", user));
 });
 
+const getChannelDetails = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is required");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: { username: username?.toLowerCase() },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers",
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo",
+            },
+        },
+        {
+            $addFields: {
+                subscribersCount: { $size: "$subscribers" },
+                subscribedToCount: { $size: "$subscribedTo" },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, "$subscribers.subscriber"],
+                        },
+                        then: true,
+                        else: false,
+                    },
+                },
+            },
+        },
+        {
+            $project: {
+                // what data to send
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                subscribedToCount: 1,
+                avatar: 1,
+                coverImage: 1,
+                isSubscribed: 1,
+                email: 1,
+            },
+        },
+    ]);
+
+    console.log(channel); // data returned is in form of array
+
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Channel found", channel[0]));
+});
+
 const renewToken = asyncHandler(async (req, res) => {
     try {
         // get refresh token from cookies
@@ -351,4 +419,5 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
+    getChannelDetails,
 };
