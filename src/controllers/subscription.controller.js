8d8ca2d9fd758ac5091d354bbb7f1f66f49966d4
Subham Mani <manisubham09@gmail.com>
2024-03-16 11:47:09 +0530

    Add getUserChannelSubscriber and getSubscribedChannels functions

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -3,6 +3,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params;
@@ -49,4 +50,92 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     }
 });
 
-export { toggleSubscription };
+const getUserChannelSubscriber = asyncHandler(async (req, res) => {
+    const { channelId } = req.params;
+
+    const subscribersList = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                subscriber: { $first: "$subscriber" },
+            },
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                "Subscribers fetched sucessfully",
+                subscribersList
+            )
+        );
+});
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params;
+
+    const subscribedToList = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channel",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullName: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                channel: { $first: "$channel" },
+            },
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                "Subscribed channels fetched sucessfully",
+                subscribedToList
+            )
+        );
+});
+
+export { toggleSubscription, getUserChannelSubscriber, getSubscribedChannels };
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+    getSubscribedChannels,
+    getUserChannelSubscriber,
+    toggleSubscription,
+} from "../controllers/subscription.controller.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const subscriptionRouter = Router();
@@ -7,5 +11,13 @@ const subscriptionRouter = Router();
 subscriptionRouter
     .route("/toggle-subscription/:channelId")
     .post(verifyToken, toggleSubscription);
+
+subscriptionRouter
+    .route("/all-subscribers/:channelId")
+    .get(getUserChannelSubscriber);
+
+subscriptionRouter
+    .route("/subscribed-to/:subscriberId")
+    .get(getSubscribedChannels);
 
 export default subscriptionRouter;
